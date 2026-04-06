e658f1268cd757e642ea0cb1e086590e20cceeb0
itsleivey <bangalarga29@gmail.com>
2026-04-06 10:41:47 +0800

    main

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // Register Service Worker for PWA
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
+   navigator.serviceWorker.register('./service-worker.js')
       .then(registration => {
         console.log('Service Worker registered successfully:', registration);
       })
--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,17 @@
-const CACHE_NAME = "portfolio-cache-v1";
+const CACHE_NAME = "portfolio-cache-v2";
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/about.html",
-  "/projects.html",
-  "/style.css",
-  "/script.js",
-  "/images/Profile.jpg",
-  "/images/project-1.jpg",
-  "/images/project-2.jpg",
-  "/images/project-3.jpg",
-  "/images/project-4.jpg"
+  "./index.html",
+  "about.html",
+  "projects.html",
+  "style.css",
+  "script.js",
+  "images/Profile.jpg",
+  "images/dump1.jpg",
+  "images/dump2.jpg",
+  "images/project-1.jpg",
+  "images/project-2.jpg",
+  "images/project-3.jpg",
+  "images/project-4.jpg"
 ];
 
 // Install Service Worker and Cache Files
@@ -26,8 +27,7 @@ self.addEventListener("install", event => {
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
-      // Return the cached file if found, otherwise fetch from the internet
       return response || fetch(event.request);
-      })
+    })
   );
 });
